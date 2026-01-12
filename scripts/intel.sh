3d79613cca7e1fe7627e3be8f3877381e4f7dd62
alilo113 <aliopdandan@gmail.com>
2026-01-12 10:01:24 +0100

    f

--- scripts/intel.sh
+++ scripts/intel.sh
@@ -26,6 +26,29 @@ review_webserver_metadata_for_information_leakage(){
         | sed 's/<loc>//;s/<\/loc>//' \
         | sort -u \
         > ../data/intel/sitemap_urls.txt
+
+    echo "[+] Reviewing webserver metadata for information leakage from security.txt"
+    cat ../data/discovery/*_live.txt \
+        | while read -r url; do
+            securitytxt_url="${url%/}/.well-known/security.txt"
+            echo "[*] Fetching $securitytxt_url"
+            wget --no-verbose "$securitytxt_url" && cat security.txt
+        done \
+        | grep -Eo 'Contact: (mailto:[a-zA-Z0-9_.@-]+)' \
+        | sed 's/Contact: //' \     
+        | sort -u \
+        > ../data/intel/securitytxt_contacts.txt
+    
+    echo "[+] Reviewing webserver metadata for information leakage from humans.txt"
+    cat ../data/discovery/*_live.txt \
+        | while read -r url; do
+            humanstxt_url="${url%/}/humans.txt"
+            echo "[*] Fetching $humanstxt_url"
+            wget --no-verbose "$humanstxt_url" && cat humans.txt
+        done \
+        | grep -Eo '([a-zA-Z0-9_.@-]+@[a-zA-Z0-9_.-]+)' \
+        | sort -u \
+        > ../data/intel/humanstxt_contacts.txt
 }
 
 collect_js_files() {
