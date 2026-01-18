139eb087b91df4f3786bd217e9f93ade01a9a010
alilo113 <aliopdandan@gmail.com>
2026-01-18 11:24:25 +0100

    f

--- commands/root.go
+++ commands/root.go
@@ -12,7 +12,10 @@ var rootCmd = &cobra.Command{
 	Use: "Darthnet",
 	Long: "Darthnet is a CLI tool for reconnaissance and information gathering. during engagements, it can be used to collect data about targets, identify vulnerabilities, and assist in penetration testing activities.",
 	Run: func(cmd*cobra.Command, args []string) {
-
+		if discovery{
+			runDiscoveryScript(discovery)
+			return
+		}
 	}
 }
 
